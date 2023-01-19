171b09f14d3bd55cc9b0cf34ed4fbd1a0ab0f06b
sojinjang <jsojin6119@gmail.com>
2023-01-20 03:17:21 +0900

    [#1] :lipstick: style: 안내문구 컴포넌트 추가

--- client/src/pages/DiaryList.tsx
+++ client/src/pages/DiaryList.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import tw from "tailwind-styled-components";
+import { PinkPurpleBackground } from "src/components/common/Backgrounds";
+
+const Guidance = tw.p`
+  font-[jua] text-purple-1000 font-normal text-center text-3xl mt-[60px]
+`;
 
 const DiaryList = () => {
-  return <>DiaryList</>;
+  return (
+    <PinkPurpleBackground>
+      <Guidance>
+        일기장을 클릭해
+        <br />
+        가까운 사람들과
+        <br />
+        일기를 공유해보세요
+      </Guidance>
+    </PinkPurpleBackground>
+  );
 };
 
 export default DiaryList;
