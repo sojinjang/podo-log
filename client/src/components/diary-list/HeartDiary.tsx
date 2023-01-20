d7b39bb5c6af8f5a26b8b1642629fa74e0b3755b
sojinjang <jsojin6119@gmail.com>
2023-01-21 04:43:37 +0900

    [#1] :lipstick: style: diary icon hover 효과 추가

--- client/src/components/diary-list/HeartDiary.tsx
+++ client/src/components/diary-list/HeartDiary.tsx
@@ -5,15 +5,16 @@ import heartDiaryImg from "../../assets/icons/notebook_love.png";
 
 export const HeartDiary = () => {
   const navigate = useNavigate();
-
   const onClickImg = () => {
     navigate(PRIVATE_ROUTE.newDiary.path);
   };
+
   return (
     <img
       src={heartDiaryImg}
+      alt="diary"
       onClick={onClickImg}
-      className="mx-auto mt-[6vh] w-[25vh] h-[25vh] cursor-pointer"
+      className="mx-auto mt-[6vh] w-[20vh] h-[20vh] cursor-pointer max-w-xs hover:scale-105 transition duration-500 ease-in-out"
     />
   );
 };
