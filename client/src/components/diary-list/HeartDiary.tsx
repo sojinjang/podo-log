180a05bc1cdbcc9a2a170faab797442ecce29bf4
sojinjang <jsojin6119@gmail.com>
2023-01-20 18:12:10 +0900

    [#1] :sparkles: feat: 아이콘 클릭 시 새 일기장 만들기 페이지로 이동시키기

--- client/src/components/diary-list/HeartDiary.tsx
+++ client/src/components/diary-list/HeartDiary.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import heartDiaryImg from "../../assets/icons/notebook_love.png";
 
 export const HeartDiary = () => {
+  const navigate = useNavigate();
+
+  const onClickImg = () => {
+    navigate("/new-diary");
+  };
   return (
-    <img src={heartDiaryImg} className="mx-auto mt-[6vh] w-[25vh] h-[25vh] cursor-pointer" />
+    <img
+      src={heartDiaryImg}
+      onClick={onClickImg}
+      className="mx-auto mt-[6vh] w-[25vh] h-[25vh] cursor-pointer"
+    />
   );
 };
