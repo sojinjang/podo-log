7dea80100e5a29f9afb3d05a79d798a944083660
sojinjang <jsojin6119@gmail.com>
2023-01-20 23:02:51 +0900

    [#1] :lipstick: style(tailwind): tailwind custom color 추가

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         "bg-middle-purple": "#E8DBFF",
         "bg-purple": "#F6F1FF",
         "purple-1000": "#BB86FC",
+        "gray-1000": "#959595",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
