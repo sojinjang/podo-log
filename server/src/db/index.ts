7b838097cbe16c8d83bda8fce96166a3cf0163b6
상준 안 <ahosang0056@gmail.com>
2023-01-21 18:06:17 +0900

    [#8] :loud_sound: chore: console.log -> pino

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,5 +1,6 @@
 import mysql from "mysql2/promise";
 import mysqlConfig from "../config/mysql.config";
+import { logger } from "./../utils/pino";
 
 const env = process.env.NODE_ENV || "local";
 const config = mysqlConfig(env);
@@ -8,5 +9,5 @@ export const pool = mysql.createPool(config);
 
 pool
   .getConnection()
-  .then(() => console.log("정상적으로 db 접속에 성공하였습니다."))
-  .catch((err) => console.log("db 접속에 실패하였습니다.", err));
+  .then(() => logger.info("정상적으로 db 접속에 성공하였습니다."))
+  .catch((err) => logger.info("db 접속에 실패하였습니다.", err));
--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,1 +1,4 @@
+export * from "./async-handler";
 export * from "./pino";
+export * from "./build-query";
+export * from "./validator";
