cc45b8f33ddc17287b4827f4ca4b1024f30ef78a
sojinjang <jsojin6119@gmail.com>
2023-01-22 20:29:28 +0900

    [#1] :lipstick: style: nav bar 제외한 부분에 본문 내용 생성, 스크롤 가능하도록 수정

--- client/src/pages/DiaryList.tsx
+++ client/src/pages/DiaryList.tsx
@@ -8,15 +8,17 @@ import { Navbar } from "src/components/common/NavBar";
 const DiaryList = () => {
   return (
     <PinkPurpleBackground>
-      <Guidance>
-        일기장을 클릭해
-        <br />
-        가까운 사람들과
-        <br />
-        일기를 공유해보세요
-      </Guidance>
-      <PointingFinger />
-      <HeartDiary />
+      <div className="h-[calc(100vh-130px)] overflow-y-scroll">
+        <Guidance>
+          일기장을 클릭해
+          <br />
+          가까운 사람들과
+          <br />
+          일기를 공유해보세요
+        </Guidance>
+        <PointingFinger />
+        <HeartDiary />
+      </div>
       <Navbar />
     </PinkPurpleBackground>
   );
