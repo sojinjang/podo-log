d01b4b0b1112d0ebe0382c2f0d2babb01503a378
sojinjang <jsojin6119@gmail.com>
2023-01-22 23:22:53 +0900

    [#2] :sparkles: feat: user diary 정보 받아와서 state 설정하기

--- client/src/pages/DiaryList.tsx
+++ client/src/pages/DiaryList.tsx
@@ -1,11 +1,46 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 import { PinkPurpleBackground } from "src/components/common/Backgrounds";
 import { Guidance } from "src/components/diary-list/Guidance";
 import { PointingFinger } from "src/components/diary-list/PointingFinger";
 import { HeartDiary } from "src/components/diary-list/HeartDiary";
 import { Navbar } from "src/components/common/NavBar";
+
+interface DiaryInfo {
+  readonly bookId: number;
+  readonly bookName: string;
+  readonly numPpl: number;
+  readonly color: string;
+}
+
 const DiaryList = () => {
+  const [userDiaryArr, setUserDiaryArr] = useState<DiaryInfo[] | undefined>([]);
+  const [isEmpty, setIsEmpty] = useState<boolean>(true);
+
+  async function getUserDiaryArr() {
+    try {
+      // TODO: api 완성되는대로 db에서 받아온 데이터 return하도록 변경하기 23.01.22
+      const userDiaryList: DiaryInfo[] = [
+        { bookId: 1, bookName: "울 빼밀리", numPpl: 4, color: "008fff" },
+        { bookId: 2, bookName: "with 희찬 💖", numPpl: 2, color: "e054b8" },
+        { bookId: 3, bookName: "집단적독백방 💬", numPpl: 5, color: "82af20" },
+      ];
+      return userDiaryList;
+    } catch (err: any) {
+      alert(err.message);
+    }
+  }
+  const handleUserDiaryArr = async () => {
+    const userDiaries = await getUserDiaryArr();
+    setUserDiaryArr(userDiaries);
+    setIsEmpty(userDiaryArr?.length === 0 ? true : false);
+  };
+
+  useEffect(() => {
+    handleUserDiaryArr;
+  }, []);
+
   return (
     <PinkPurpleBackground>
       <div className="h-[calc(100vh-130px)] overflow-y-scroll">
