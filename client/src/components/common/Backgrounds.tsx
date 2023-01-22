d3ba4c3445f00cf99e806f310d8d13db6b0e9f24
sojinjang <jsojin6119@gmail.com>
2023-01-22 20:53:16 +0900

    [#1] :lipstick: style(background): 배경화면 색상 코드 변경

--- client/src/components/common/Backgrounds.tsx
+++ client/src/components/common/Backgrounds.tsx
@@ -1,5 +1,5 @@
 import tw from "tailwind-styled-components";
 
 export const PinkPurpleBackground = tw.div`
-  w-[calc(100vh/16*9)] h-screen m-auto bg-gradient-radial from-bg-pink via-middle-purple to-bg-purple
+  w-[calc(100vh/16*9)] h-screen m-auto bg-gradient-radial from-bg-pink via-bg-middle-purple to-bg-purple
 `;
