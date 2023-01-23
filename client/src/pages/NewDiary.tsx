84ad17480d5f80f274c0a8abd3554f5b19916daf
sojinjang <jsojin6119@gmail.com>
2023-01-24 02:32:22 +0900

    [#14] :lipstick: style: 일기장 만들기 페이지 upper section 조립

--- client/src/pages/NewDiary.tsx
+++ client/src/pages/NewDiary.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+import { PinkPurpleBackground } from "src/components/common/Backgrounds";
+import BackButton from "src/components/common/BackButton";
+import PageTitle from "src/components/common/PageTitle";
+
 const NewDiary = () => {
-  return <>새 일기장 만들기</>;
+  return (
+    <PinkPurpleBackground>
+      <BackButton />
+      <PageTitle title="일기장 만들기"></PageTitle>
+    </PinkPurpleBackground>
+  );
 };
 
 export default NewDiary;
