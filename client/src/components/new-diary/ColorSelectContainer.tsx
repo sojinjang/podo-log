3d531ea81074a057f6ba386bd0266c923bb19e49
sojinjang <jsojin6119@gmail.com>
2023-01-24 22:40:43 +0900

    [#14] :sparkles: feat: color 선택할 때 state 변경, 선택된 color만 dashed border로 보여주기

--- client/src/components/new-diary/ColorSelectContainer.tsx
+++ client/src/components/new-diary/ColorSelectContainer.tsx
@@ -1,26 +1,50 @@
 import React from "react";
 import tw from "tailwind-styled-components";
+import { useRecoilState } from "recoil";
+
+import { selectedColorAtom } from "../../recoil/new-diary";
+
+const DIARY_COLOR = [
+  "000000",
+  "008fff",
+  "50e3c2",
+  "82af20",
+  "6200ee",
+  "018786",
+  "309054",
+  "b00020",
+  "2c3f50",
+  "e054b8",
+  "e39801",
+  "ff5436",
+];
 
 const ColorSelectButton = tw.div`
-    md:w-[65px] w-[35px] md:h-[65px] h-[35px] rounded-full m-auto mb-[2vh] cursor-pointer
+md:w-[65px] w-[35px] md:h-[65px] h-[35px] rounded-full m-auto mb-[2vh] cursor-pointer
 `;
+
+const renderColorSelectButtons = () => {
+  const [selectedColor, setSelectedColor] = useRecoilState(selectedColorAtom);
+
+  const colorSelectButtons = DIARY_COLOR.map((color) => {
+    const isSelected = color === selectedColor;
+    const borderStyle = isSelected ? "border-white border-dashed border-[6px]" : "border-none";
+    return (
+      <ColorSelectButton
+        key={color}
+        id={color}
+        className={`bg-[#${color}] ${borderStyle}`}
+        onClick={() => setSelectedColor(color)}
+      />
+    );
+  });
+
+  return colorSelectButtons;
+};
 
 const ColorSelectContainer = () => {
   return (
-    <div className="grid grid-cols-6 mt-[1.5vh] px-[5vw]">
-      <ColorSelectButton className="bg-[#000000]" />
-      <ColorSelectButton className="bg-[#008fff]" />
-      <ColorSelectButton className="bg-[#50e3c2]" />
-      <ColorSelectButton className="bg-[#82af20]" />
-      <ColorSelectButton className="bg-[#6200ee]" />
-      <ColorSelectButton className="bg-[#018786]" />
-      <ColorSelectButton className="bg-[#309054]" />
-      <ColorSelectButton className="bg-[#b00020]" />
-      <ColorSelectButton className="bg-[#2c3f50]" />
-      <ColorSelectButton className="bg-[#e054b8]" />
-      <ColorSelectButton className="bg-[#e39801]" />
-      <ColorSelectButton className="bg-[#ff5436]" />
-    </div>
+    <div className="grid grid-cols-6 mt-[1.5vh] px-[5vw]">{renderColorSelectButtons()}</div>
   );
 };
 
