70d53bb4fb6e5df01390c3d1f1cff754515b2313
sojinjang <jsojin6119@gmail.com>
2023-01-24 22:41:53 +0900

    [#14] :sparkles: feat: 선택된 색상에 따라 다이어리 아이콘 색상 변경

--- client/src/components/new-diary/DiaryIcon.tsx
+++ client/src/components/new-diary/DiaryIcon.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import tw from "tailwind-styled-components";
+import { useRecoilValue } from "recoil";
 
-interface DiaryIconProps {
-  readonly color: string;
-}
+import { selectedColorAtom } from "../../recoil/new-diary";
 
 const DiaryImg = tw.img`
     mx-auto pt-[9vh] h-[27vh] md:h-[25vh]
 `;
 
-const DiaryIcon = ({ color }: DiaryIconProps) => {
-  return <DiaryImg src={require(`../../assets/icons/diary/${color}.png`)} />;
+const DiaryIcon = () => {
+  const selectedColor = useRecoilValue(selectedColorAtom);
+  return <DiaryImg src={require(`../../assets/icons/diary/${selectedColor}.png`)} />;
 };
 
 export default DiaryIcon;
