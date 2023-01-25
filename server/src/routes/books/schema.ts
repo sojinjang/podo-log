2a7c717828f81272f5ea31fedb96c798904ebf97
상준 안 <ahosang0056@gmail.com>
2023-01-25 18:07:32 +0900

    [#18] :sparkles: feat(book): validate 추가

---
+++ server/src/routes/books/schema.ts
@@ -0,0 +1,15 @@
+import Joi from "joi";
+
+export default {
+  createBook: Joi.object().keys({
+    bookName: Joi.string().required().min(1).max(20),
+    color: Joi.string().required(),
+  }),
+  bookId: Joi.object().keys({
+    bookId: Joi.number().required(),
+  }),
+  patchBook: Joi.object().keys({
+    bookName: Joi.string().optional().min(1).max(20),
+    color: Joi.string().optional(),
+  }),
+};
