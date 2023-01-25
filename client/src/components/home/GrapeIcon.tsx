2e94b7dfee6efa9a1a2989cae6c9ee38f2ce00ea
sojinjang <jsojin6119@gmail.com>
2023-01-26 04:00:36 +0900

    [#23] :lipstick: style: 포도 아이콘 배치, 컴포넌트 추가

---
+++ client/src/components/home/GrapeIcon.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import grapeImg from "../../assets/grape/grape_8.png";
+
+export const GrapeIcon = () => {
+  return (
+    <img src={grapeImg} alt="grape" className="m-auto mt-[8vh] w-[23vh] h-[23vh] opacity-95" />
+  );
+};
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import { DefaultBackground } from "src/components/common/Backgrounds";
 import "src/components/common/Backgrounds.css";
 import { Greeting } from "src/components/home/Greeting";
+import { GrapeIcon } from "src/components/home/GrapeIcon";
 
 const Home = () => {
   return (
     <DefaultBackground className="animated-gradient">
       <Greeting />
+      <GrapeIcon />
     </DefaultBackground>
   );
 };
