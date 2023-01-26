1138ff30b5fc21f833d41d71fe87311d92ecf631
상준 안 <ahosang0056@gmail.com>
2023-01-26 17:39:15 +0900

    [#24] :truck: chore(diary): 경로추가

--- server/src/routes/books/index.ts
+++ server/src/routes/books/index.ts
@@ -2,11 +2,17 @@ import { Router } from "express";
 import { bookController } from "./book-controller";
 import validator, { ValidationSource } from "../../utils/validator";
 import schema from "./schema";
+import { getDiariesRouter } from "../diaries";
 
 const router = Router();
 
 router.post("/", validator(schema.createBook), bookController.create);
 router.get("/", bookController.getByUserId);
+router.use(
+  "/:bookId/diaries",
+  validator(schema.bookId, ValidationSource.PARAM),
+  getDiariesRouter
+);
 router.patch(
   "/:bookId",
   validator(schema.bookId, ValidationSource.PARAM),
