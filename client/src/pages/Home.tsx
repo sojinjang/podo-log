b502c866140273d653f7ae0d9550caafb6329a72
sojinjang <jsojin6119@gmail.com>
2023-01-26 23:40:17 +0900

    [#23] :lipstick: style: 홈화면 로그인 섹션 컴포넌트 조립, 배치

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,17 +1,28 @@
 import React from "react";
+import tw from "tailwind-styled-components";
 
 import { DefaultBackground } from "src/components/common/Backgrounds";
 import "src/components/common/Backgrounds.css";
 import { Greeting } from "src/components/home/Greeting";
 import { GrapeIcon } from "src/components/home/GrapeIcon";
 import EmailLoginContainer from "../components/home/EmailLoginContainer";
+import SNSLoginContainer from "../components/home/SNSLoginContainer";
+import SignUpButton from "src/components/home/SignUpButton";
 
+const LoginSection = tw.div`
+backdrop-blur-3xl border-4 bg-slate-50/5 border-slate-50/80 rounded-xl 
+w-[80%] py-[2vh] mt-[2vh] min-[390px]:mt-[6vh] mx-auto
+`;
 const Home = () => {
   return (
     <DefaultBackground className="animated-gradient">
       <Greeting />
       <GrapeIcon />
-      <EmailLoginContainer />
+      <LoginSection>
+        <EmailLoginContainer />
+        <SNSLoginContainer />
+        <SignUpButton />
+      </LoginSection>
     </DefaultBackground>
   );
 };
