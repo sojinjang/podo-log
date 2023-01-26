9d0fbc349c4b52f2068e4fdfb49e78ee6449f717
sojinjang <jsojin6119@gmail.com>
2023-01-26 21:59:43 +0900

    [#23] :lipstick: style: 홈화면 이메일 로그인 컨테이너 추가

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,12 +4,14 @@ import { DefaultBackground } from "src/components/common/Backgrounds";
 import "src/components/common/Backgrounds.css";
 import { Greeting } from "src/components/home/Greeting";
 import { GrapeIcon } from "src/components/home/GrapeIcon";
+import EmailLoginContainer from "../components/home/EmailLoginContainer";
 
 const Home = () => {
   return (
     <DefaultBackground className="animated-gradient">
       <Greeting />
       <GrapeIcon />
+      <EmailLoginContainer />
     </DefaultBackground>
   );
 };
