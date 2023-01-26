71f30c6f07c976d87b0940497886fc374703918d
sojinjang <jsojin6119@gmail.com>
2023-01-27 03:13:38 +0900

    [#23] :sparkles: feat: 로그인 상태면 로그인 창 보이지 않게 하고 4초 뒤 일기장 리스트로 이동

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import tw from "tailwind-styled-components";
 import Fade from "react-reveal/Fade";
 
+import { getCookieValue } from "src/utils/cookie";
+import { Keys } from "src/constants/Keys";
+import { PRIVATE_ROUTE } from "src/router/ROUTE_INFO";
 import { DefaultBackground } from "src/components/common/Backgrounds";
 import "src/components/common/Backgrounds.css";
 import { Greeting } from "src/components/home/Greeting";
@@ -15,19 +19,31 @@ backdrop-blur-3xl border-4 bg-slate-50/5 border-slate-50/80 rounded-xl
 w-[80%] py-[2vh] mt-[2vh] min-[390px]:mt-[6vh] mx-auto
 `;
 const Home = () => {
+  const navigate = useNavigate();
+  const isLoggedIn = Boolean(getCookieValue(Keys.ACCESS_TOKEN));
+  const moveToDiaries = () => {
+    setTimeout(() => navigate(PRIVATE_ROUTE.diaries.path), 4000);
+  };
+
+  useEffect(() => {
+    if (isLoggedIn) return moveToDiaries;
+  }, []);
+
   return (
     <DefaultBackground className="animated-gradient">
       <Fade duration={3000}>
         <Greeting />
         <GrapeIcon />
       </Fade>
-      <Fade bottom duration={3000}>
-        <LoginSection>
-          <EmailLoginContainer />
-          <SNSLoginContainer />
-          <SignUpButton />
-        </LoginSection>
-      </Fade>
+      {!isLoggedIn && (
+        <Fade bottom duration={3000}>
+          <LoginSection>
+            <EmailLoginContainer />
+            <SNSLoginContainer />
+            <SignUpButton />
+          </LoginSection>
+        </Fade>
+      )}
     </DefaultBackground>
   );
 };
