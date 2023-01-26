0f3c1aefe58fab4f9d8dcce47541c2b51216bb92
상준 안 <ahosang0056@gmail.com>
2023-01-26 17:38:06 +0900

    [#24] :sparkles: feat(diary): validate

---
+++ server/src/routes/diaries/schema.ts
@@ -0,0 +1,18 @@
+import Joi from "joi";
+
+export default {
+  createDiary: Joi.object().keys({
+    title: Joi.string().required().max(40, "utf8"),
+    content: Joi.string().required().max(580, "utf8"),
+  }),
+  diaryId: Joi.object().keys({
+    diaryId: Joi.number().required(),
+  }),
+  bookId: Joi.object().keys({
+    bookId: Joi.number().required(),
+  }),
+  patchDiary: Joi.object().keys({
+    title: Joi.string().optional().max(40, "utf8"),
+    content: Joi.string().optional().max(580, "utf8"),
+  }),
+};
--- server/src/routes/login/schema.ts
+++ server/src/routes/login/schema.ts
@@ -3,8 +3,8 @@ import { JoiAuthBearer } from "../../utils";
 
 export default {
   local: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+    email: Joi.string().required().email().max(25),
+    password: Joi.string().required().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).max(55),
   }),
   userId: Joi.object().keys({
     userId: Joi.number().required(),
