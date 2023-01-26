acb6b40dab208261bee4382e3a7744dbe5e56834
sojinjang <jsojin6119@gmail.com>
2023-01-27 01:22:30 +0900

    [#23] :sparkles: feat: 회원가입 버튼 클릭 시 회원가입 페이지로 이동

--- client/src/components/home/SignUpButton.tsx
+++ client/src/components/home/SignUpButton.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import tw from "tailwind-styled-components";
+import { PUBLIC_ROUTE } from "src/router/ROUTE_INFO";
 
 const SignUpPhrase = tw.p`
 font-[notosans] font-semibold text-slate-50 text-sm sm:text-lg
@@ -7,7 +9,11 @@ cursor-pointer underline drop-shadow-lg shadow-black text-center
 `;
 
 const SignUpButton = () => {
-  return <SignUpPhrase>Sign Up</SignUpPhrase>;
+  const navigate = useNavigate();
+  const onClickSignUp = () => {
+    navigate(PUBLIC_ROUTE.signUp.path);
+  };
+  return <SignUpPhrase onClick={onClickSignUp}>Sign Up</SignUpPhrase>;
 };
 
 export default SignUpButton;
