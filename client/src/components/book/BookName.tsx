6a417bff050f8d9ba9258f6203b3a37db68df385
sojinjang <jsojin6119@gmail.com>
2023-01-27 21:28:38 +0900

    [#28] :iphone: style: 반응형 작업, align 맞추기

--- client/src/components/book/BookName.tsx
+++ client/src/components/book/BookName.tsx
@@ -4,7 +4,7 @@ import tw from "tailwind-styled-components";
 import PageTitle from "../common/PageTitle";
 
 const NumMember = tw.div`
-font-[jua] text-gray-1000 text-[3vh] md:text-[2.5vh] mx-3 my-auto pt-[15px]
+font-[jua] text-gray-1000 text-[3vh] md:text-[2.7vh] mx-3 my-auto pt-[1.2vh]
 `;
 
 const BookName = () => {
