cf0f7c756302729aa0a14b278f0955024799fc38
상준 안 <ahosang0056@gmail.com>
2023-01-27 17:25:46 +0900

    [#15] :wrench: chore(login): 쿠키 옵션 추가

--- server/src/config/default.config.ts
+++ server/src/config/default.config.ts
@@ -1,7 +1,26 @@
+import { CookieOptions } from "express";
+
 export const environment = process.env.NODE_ENV as string;
 export const port = process.env.PORT;
+export const podologURL = process.env.PODOLOG_URL;
 
-export const corsOption = { origin: process.env.CORS_URL, optionsSuccessStatus: 200 };
+function setCookieTime(time: number, unit: "d" | "h" | "m") {
+  const expiredM = unit === "m" ? time : unit === "h" ? time * 60 : time * 24 * 60;
+  let todayDate = new Date();
+  todayDate.setTime(todayDate.getTime() + expiredM * 60 * 1000);
+  return todayDate;
+}
+
+export const cookieOption = (time: number, unit: "d" | "h" | "m"): CookieOptions => {
+  const expires = setCookieTime(time, unit);
+  return { httpOnly: true, path: podologURL, sameSite: "lax", expires };
+};
+
+export const corsOption = {
+  origin: podologURL,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
 export const jwtSecretKey = process.env.JWT_SECRET_KEY as string;
 
