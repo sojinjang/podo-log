9346224cfe4ad6bcfeab9e41d2cc05d6249aa6fc
sojinjang <jsojin6119@gmail.com>
2023-01-29 02:21:50 +0900

    [#28] :sparkles: feat(utils): useEffect 첫 렌더링 시 함수 실행 막는 custom hook 추가

---
+++ client/src/utils/hooks.js
@@ -0,0 +1,10 @@
+import { useRef, useEffect } from "react";
+
+export const useDidMountEffect = (func, deps) => {
+  const didMount = useRef(false);
+
+  useEffect(() => {
+    if (didMount.current) func();
+    else didMount.current = true;
+  }, deps);
+};
