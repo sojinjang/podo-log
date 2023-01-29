9287f46f6ef5eafbc622628a58b618e04cfcf020
sojinjang <jsojin6119@gmail.com>
2023-01-30 02:42:23 +0900

    [#28] :sparkles: feat: useLocation 훅으로 파라미터(제목, 인원 수) 전달받아 표시

--- client/src/components/book/BookName.tsx
+++ client/src/components/book/BookName.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import tw from "tailwind-styled-components";
+import { useLocation } from "react-router-dom";
 
 import PageTitle from "../common/PageTitle";
 
@@ -8,10 +9,14 @@ font-[jua] text-gray-1000 text-[3vh] md:text-[2.7vh] mx-3 my-auto pt-[1.2vh]
 `;
 
 const BookName = () => {
+  const location = useLocation();
+  const bookName = location.state.name;
+  const numMembers = location.state.numMembers;
+
   return (
     <div className="flex justify-center">
-      <PageTitle title="울 빼밀리 👨‍👩‍👧‍👧" />
-      <NumMember>4</NumMember>
+      <PageTitle title={bookName} />
+      <NumMember>{numMembers}</NumMember>
     </div>
   );
 };
