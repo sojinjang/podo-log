19241181a315d741a1b5a72594a95054a73aac94
sojinjang <jsojin6119@gmail.com>
2023-01-30 00:58:12 +0900

    [#28] :lipstick: style: diary list 스크롤 설정

--- client/src/pages/Book.tsx
+++ client/src/pages/Book.tsx
@@ -5,11 +5,11 @@ import BackButton from "../components/common/BackButton";
 import BookName from "../components/book/BookName";
 import NewDiaryButton from "src/components/book/NewDiaryButton";
 import SettingButton from "src/components/book/SettingButton";
-import DiaryListContainer from "src/components/book/DiaryListContainer";
+import { DiaryListContainer } from "src/components/book/DiaryListContainer";
 
 const Book = () => {
   return (
-    <PinkPurpleBackground>
+    <PinkPurpleBackground className="overflow-y-scroll">
       <BackButton />
       <SettingButton />
       <BookName />
