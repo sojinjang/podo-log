1432474aaa51681425f93ddc811204c5c6286e40
sojinjang <jsojin6119@gmail.com>
2023-01-30 22:06:37 +0900

    [#40] :sparkles: feat: 회원가입 페이지 컴포넌트 생성, 소셜 회원가입 컴포넌트 추가

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
+
+import { DefaultBackground } from "src/components/common/Backgrounds";
+import "src/components/common/Backgrounds.css";
+import { Greeting } from "src/components/home/Greeting";
+import SNSLoginContainer from "../components/home/SNSLoginContainer";
 
 const SignUp = () => {
-  return <>SignUp</>;
+  return (
+    <DefaultBackground className="animated-gradient">
+      <Greeting />
+      <Fade bottom duration={3000}>
+        <SNSLoginContainer sectionTitle="SNS 계정 회원가입" />
+      </Fade>
+    </DefaultBackground>
+  );
 };
 
 export default SignUp;
