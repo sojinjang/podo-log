0370853a4e84b20a15a6b6d019e982b139d1430a
상준 안 <ahosang0056@gmail.com>
2023-01-31 14:51:21 +0900

    [#34] :bug: fix(image): multer 함수 개선
    편의상 URL 과 s3-Key 값을 같게하기 위해 파일이름을 정규식으로 정리

--- server/src/middlewares/multer-s3.ts
+++ server/src/middlewares/multer-s3.ts
@@ -4,14 +4,20 @@ import multer from "multer";
 import multerS3 from "multer-s3";
 import { logger } from "../utils";
 import { awsS3ClientConfig, awsS3Config, bucketName } from "../config/aws-s3.config";
-import { S3Client } from "@aws-sdk/client-s3";
+import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { Request } from "express";
 
 const s3 = new AWS.S3(awsS3Config);
 const s3Client = new S3Client(awsS3ClientConfig);
 const allowedExtensions = [".png", ".jpg", ".jpeg", ".bmp"];
 
-const uploadDirectory = () => {};
+const makeKeyFromURL = (location: string) => {
+  return location.split("/").slice(-2).join("/");
+};
+
+const uploadDirectory = (reqUri: string) => {
+  reqUri.split("/");
+};
 
 const imageUploader = multer({
   storage: multerS3({
@@ -20,30 +26,28 @@ const imageUploader = multer({
     key: (req: Request, file, callback) => {
       const uploadDirectory = req.query.directory ?? "default";
       const extension = path.extname(file.originalname);
+      const filename = file.originalname.replace(/([^\w\.]*)/g, "");
       if (!allowedExtensions.includes(extension)) {
         return callback(new Error("wrong extension"));
       }
-      callback(null, `${uploadDirectory}/${Date.now()}_${file.originalname}`);
+      callback(null, `${uploadDirectory}/${Date.now()}_${filename}`);
     },
     acl: "public-read-write",
   }),
   limits: { fileSize: 5 * 1024 * 1024 },
 });
 
-const imageDeleter = (location: string) => {
+const imageDeleter = async (location: string) => {
   let params = {
     Bucket: bucketName,
-    Key: location.split("/").slice(-2).join("/"),
+    Key: makeKeyFromURL(location),
   };
-
   try {
-    s3.deleteObject(params, function (error, data) {
-      if (error) {
-        logger.error("err: ", error, error.stack);
-      } else {
-        logger.info(" 정상 삭제 되었습니다.");
-      }
-    });
+    console.log(params.Key);
+    const data = await s3Client.send(new DeleteObjectCommand(params));
+
+    logger.info(`정상 삭제 되었습니다.`);
+    logger.info(data);
   } catch (err) {
     logger.error(err);
     throw err;
