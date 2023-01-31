0a5ec0b585aefbeda95db3d20c0b0c58355f5199
sojinjang <jsojin6119@gmail.com>
2023-02-01 00:06:38 +0900

    [#39] :sparkles: feat: private page 라우팅 시 로그인 상태 확인 access token state로 하기

--- client/src/router/PrivateRouter.tsx
+++ client/src/router/PrivateRouter.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { PUBLIC_ROUTE } from "./ROUTE_INFO";
-
-// TODO: 로그인 인증 방식 정해지는대로 isLogin 로그인 상태 확인하는 함수로 변경하기 23.01.18
-const isLogin = true;
+import { useRecoilValue } from "recoil";
+import { accessTokenAtom } from "src/recoil/token";
 
 interface Element {
   children: JSX.Element;
 }
 
 export const PrivateRouter = ({ children }: Element) => {
-  if (isLogin) {
-    return <>{children}</>;
-  } else {
-    return <Navigate to={PUBLIC_ROUTE.home.path} />;
-  }
+  const isLogin = Boolean(useRecoilValue(accessTokenAtom));
+
+  if (isLogin) return <>{children}</>;
+  return <Navigate to={PUBLIC_ROUTE.home.path} />;
 };
