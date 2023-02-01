9b0c796a148cc4b849640ada65076dcf3c591da0
sojinjang <jsojin6119@gmail.com>
2023-02-01 18:09:42 +0900

    :rewind: chore: merge 실수 복구

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,48 +1,70 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import tw from "tailwind-styled-components";
-import { Link } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 
-import { API_URL } from "src/constants/API_URL";
-import naverLogo from "../../assets/icons/sns/naver.png";
-import kakaoLogo from "../../assets/icons/sns/kakao.png";
+import { accessTokenAtom } from "src/recoil/token";
+import { Token } from "src/recoil/token/atom";
+import { refreshToken, moveToDiaries } from "../utils/token";
+import { useDidMountEffect } from "src/utils/hooks";
 
-const SNSLoginContainer = () => {
+import { DefaultBackground } from "src/components/common/Backgrounds";
+import "src/components/common/Backgrounds.css";
+import { Greeting } from "src/components/home/Greeting";
+import { GrapeIcon } from "src/components/home/GrapeIcon";
+import EmailLoginContainer from "../components/home/EmailLoginContainer";
+import SNSLoginContainer from "../components/home/SNSLoginContainer";
+import SignUpButton from "src/components/home/SignUpButton";
+
+const Home = () => {
+  const ACCESS_TOKEN_EXPIRY_TIME = 3600 * 1000;
+  const REFRESH_TIME = 60000;
+  const navigate = useNavigate();
+  const [URLSearchParams] = useSearchParams();
+  const [accessToken, setAccessToken] = useRecoilState<Token>(accessTokenAtom);
+
+  // MEMO: 아래 hook을 자식 컴포넌트 (SNSLoginContainer)에서 실행하면 access token을 undefined로 인식.
+  // 리팩토링 필요 23.02.01
+  useDidMountEffect(() => {
+    const isSNSLogin = URLSearchParams.get("snslogin") === "success";
+    if (isSNSLogin) {
+      refreshToken(setAccessToken);
+      setInterval(() => refreshToken(setAccessToken), ACCESS_TOKEN_EXPIRY_TIME - REFRESH_TIME);
+    }
+
+    moveToDiaries(accessToken, navigate);
+  }, [accessToken]);
+
+  useDidMountEffect(() => {
+    moveToDiaries(accessToken, navigate);
+  }, []);
+
   return (
-    <div className="mx-auto mt-5 w-[65%]">
-      <Divider />
-      <SectionDescription>sns 계정으로 로그인하기</SectionDescription>
-      <IconContainer>
-        <SNSLoginButtonBg>
-          <SNSLoginButtonBg className="bg-[#03C75A]">
-            <Link to={process.env.REACT_APP_SERVER_URL + API_URL.naverLogin}>
-              <img src={naverLogo} />
-            </Link>
-          </SNSLoginButtonBg>
-        </SNSLoginButtonBg>
-        <SNSLoginButtonBg className="bg-[#FEE500]">
-          <Link to={process.env.REACT_APP_SERVER_URL + API_URL.kakaoLogin}>
-            <img src={kakaoLogo} />
-          </Link>
-        </SNSLoginButtonBg>
-      </IconContainer>
-    </div>
+    <DefaultBackground className="animated-gradient">
+      <Fade duration={3000}>
+        <Greeting />
+        <GrapeIcon />
+      </Fade>
+      {!accessToken && (
+        <Fade bottom duration={3000}>
+          <LoginSection>
+            <EmailLoginContainer
+              tokenExpireTime={ACCESS_TOKEN_EXPIRY_TIME}
+              refreshTime={REFRESH_TIME}
+            />
+            <SNSLoginContainer sectionTitle="SNS 계정으로 로그인하기" />
+            <SignUpButton />
+          </LoginSection>
+        </Fade>
+      )}
+    </DefaultBackground>
   );
 };
 
-export default SNSLoginContainer;
+export default Home;
 
-const SNSLoginButtonBg = tw.div`
-relative md:w-[65px] w-[40px] md:h-[65px] h-[40px] rounded-full m-auto mb-[2vh] cursor-pointer
-`;
-
-const Divider = tw.hr`
-w-full h-[3px] mx-auto mt-4 bg-slate-50/80 rounded md:mt-10
-`;
-
-const SectionDescription = tw.p`
-font-[notosans] font-semibold text-slate-50/80 text-sm sm:text-lg my-4
-`;
-
-const IconContainer = tw.div`
-flex mx-auto w-[50%]
+const LoginSection = tw.div`
+backdrop-blur-3xl border-4 bg-slate-50/5 border-slate-50/80 rounded-xl 
+w-[80%] py-[2vh] mt-[2vh] min-[390px]:mt-[6vh] mx-auto
 `;
