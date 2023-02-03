78cc79e00c41ad0af15e95d51eccee8a1375363f
sojinjang <jsojin6119@gmail.com>
2023-02-04 03:26:11 +0900

    [#35] :sparkles: feat: 일기 작성 페이지 컴포넌트들 조립

--- client/src/pages/NewDiary.tsx
+++ client/src/pages/NewDiary.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+import { PinkPurpleBackground } from "src/components/common/Backgrounds";
+import BackButton from "src/components/common/BackButton";
+import PostButton from "src/components/new-diary/PostButton";
+import PageTitle from "src/components/common/PageTitle";
+import NewDiaryForm from "src/components/new-diary/NewDiaryForm";
+
 const NewDiary = () => {
-  return <>일기 작성 페이지</>;
+  return (
+    <PinkPurpleBackground>
+      <BackButton />
+      <PostButton />
+      <PageTitle title="일기 작성" />
+      <NewDiaryForm />
+    </PinkPurpleBackground>
+  );
 };
 
 export default NewDiary;
