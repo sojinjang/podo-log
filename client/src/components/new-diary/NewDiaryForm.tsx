ce417716dfbedc01e94b26eeccce92312825b930
sojinjang <jsojin6119@gmail.com>
2023-02-04 04:27:06 +0900

    [#35] :lipstick: style: 일기 내용 폰트 굵기 수정

--- client/src/components/new-diary/NewDiaryForm.tsx
+++ client/src/components/new-diary/NewDiaryForm.tsx
@@ -72,12 +72,12 @@ export default NewDiaryForm;
 const DiaryForm = tw.form`
 h-[80%] w-[80%] bg-white/60 m-auto rounded-xl mt-[3vh] overflow-y-scroll
 `;
-const inputStyle = `font-[notosans] font-semibold
+const inputStyle = `font-[notosans] 
 bg-transparent p-4 sm:p-6 w-full`;
 
 const TitleInput = tw.input`
-text-xl sm:text-2xl
+text-xl sm:text-2xl font-semibold
 `;
 const ContentInput = tw.textarea`
-h-[30%] mt-[1vh] text-base sm:text-xl
+h-[30%] mt-[1vh] text-base sm:text-xl font-medium
 `;
