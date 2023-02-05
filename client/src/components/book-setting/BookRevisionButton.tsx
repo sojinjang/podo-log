32eed5ee398669b753ed06389975ac0b48cf76f0
sojinjang <jsojin6119@gmail.com>
2023-02-05 21:39:57 +0900

    [#88] :sparkles: feat: 일기장 수정 페이지 이동 기능, 일기장 색상/이름 넘겨주기

---
+++ client/src/components/book-setting/BookRevisionButton.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { PRIVATE_ROUTE } from "src/router/ROUTE_INFO";
+
+import ClickableContainer from "./ClickableContainer";
+import { BookIdType } from "src/pages/BookSetting";
+
+const BookRevisionButton = ({ bookId }: BookIdType) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const onClickRevisionButton = () => {
+    navigate(
+      `${PRIVATE_ROUTE.books.path}/${bookId}/${
+        PRIVATE_ROUTE.bookRevision.path.split("/").reverse()[0]
+      }
+        `,
+      {
+        state: {
+          name: location.state.name,
+          color: location.state.color,
+        },
+      }
+    );
+  };
+
+  return <ClickableContainer onClick={onClickRevisionButton}>일기장 수정</ClickableContainer>;
+};
+
+export default BookRevisionButton;
