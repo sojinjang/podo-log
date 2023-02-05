de94077d0a9a4d7c75f5b148c86ca4eae355d947
sojinjang <jsojin6119@gmail.com>
2023-02-05 20:08:20 +0900

    [#88] :sparkles: feat: 일기장 멤버 get 요청, props 전달

--- client/src/components/book-setting/BookMembersInfo.tsx
+++ client/src/components/book-setting/BookMembersInfo.tsx
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRecoilValue } from "recoil";
 import tw from "tailwind-styled-components";
 
-const BookMembersInfo = () => {
-  return <Container>일기장 공유 멤버</Container>;
+import { API_URL } from "src/constants/API_URL";
+import { get } from "src/utils/api";
+import { BookIdType } from "src/pages/BookSetting";
+import { accessTokenAtom } from "src/recoil/token";
+import { useDidMountEffect } from "src/utils/hooks";
+import MemberProfile from "./MemberProfile";
+
+export interface MemberInfo {
+  userId?: number;
+  nickname: string;
+  profile: string;
+  isMe: boolean;
+}
+
+export const BookMembersInfo = ({ bookId }: BookIdType) => {
+  const accessToken = useRecoilValue(accessTokenAtom);
+  const [members, setMembers] = useState<MemberInfo[]>([]);
+
+  const getBookmembers = async () => {
+    try {
+      const response = await get(API_URL.members(bookId), "", accessToken);
+      setMembers(response.data);
+    } catch (err) {
+      if (err instanceof Error) alert(err.message);
+    }
+  };
+
+  useDidMountEffect(() => {
+    getBookmembers();
+  }, []);
+
+  return (
+    <Container>
+      일기장 공유 멤버
+      <Divider />
+      {members.map((member) => {
+        return (
+          <MemberProfile
+            key={member.userId}
+            profile={member.profile}
+            nickname={member.nickname}
+            isMe={member.isMe}
+          />
+        );
+      })}
+    </Container>
+  );
 };
 
-export default BookMembersInfo;
-
 const Container = tw.div`
-font-[notosans] text-[1.5vh] bg-white/60 rounded-lg drop-shadow-lg
-mx-auto mb-[1.5vh] w-[90%] p-3
+font-[notosans] text-[1.7vh] bg-white/60 rounded-lg drop-shadow-lg
+mx-auto mb-[1.5vh] w-[90%] md:p-5 p-3
+`;
+
+const Divider = tw.hr`
+w-full h-[2px] mx-auto mt-1 mb-2 bg-[#C7C7C7]
 `;
