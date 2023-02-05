165c5c8e167b55565ea42ccdb4ea65bcc666afcf
sojinjang <jsojin6119@gmail.com>
2023-02-05 20:34:24 +0900

    [#88] :sparkles: feat: 일기장 나가기 확인, 나간 뒤 일기장 리스트로 이동

---
+++ client/src/components/book-setting/LeaveBookButton.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { useNavigate } from "react-router-dom";
+
+import { accessTokenAtom } from "src/recoil/token";
+import { del } from "src/utils/api";
+import { API_URL } from "src/constants/API_URL";
+import { PRIVATE_ROUTE } from "src/router/ROUTE_INFO";
+import ClickableContainer from "./ClickableContainer";
+import { BookIdType } from "src/pages/BookSetting";
+
+const LeaveBookButton = ({ bookId }: BookIdType) => {
+  const navigate = useNavigate();
+  const accessToken = useRecoilValue(accessTokenAtom);
+  const leaveBook = async () => {
+    try {
+      await del(API_URL.books, String(bookId), accessToken);
+      navigate(PRIVATE_ROUTE.books.path);
+    } catch (err) {
+      if (err instanceof Error) alert(err.message);
+    }
+  };
+  return (
+    <ClickableContainer
+      onClick={() => {
+        confirm("정말 일기장을 나가시겠습니까? 🥺");
+        leaveBook();
+      }}
+    >
+      일기장 나가기
+    </ClickableContainer>
+  );
+};
+
+export default LeaveBookButton;
