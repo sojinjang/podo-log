5dcadfaa1e42187233a2412cd8f1ba8a79192048
sojinjang <jsojin6119@gmail.com>
2023-02-05 20:33:39 +0900

    [#88] :recycle: refactor: 공통 버튼 컨테이너 컴포넌트 분리

---
+++ client/src/components/book-setting/ClickableContainer.tsx
@@ -0,0 +1,9 @@
+import tw from "tailwind-styled-components";
+
+const ClickableContainer = tw.div`
+font-[notosans] text-[1.7vh] bg-white/60 rounded-lg cursor-pointer
+shadow-lg hover:shadow-none ease-in duration-300
+mx-auto mb-[1.5vh] w-[90%] md:p-5 p-3
+`;
+
+export default ClickableContainer;
