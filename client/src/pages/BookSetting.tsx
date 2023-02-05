82b4dc7115d11a411cb80cb850919032c52bb3b0
sojinjang <jsojin6119@gmail.com>
2023-02-05 21:40:22 +0900

    [#88] :sparkles: feat: 일기장 설정 내 버튼 컴포넌트 조립, props 넘겨주기

--- client/src/pages/BookSetting.tsx
+++ client/src/pages/BookSetting.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import tw from "tailwind-styled-components";
+import { useParams } from "react-router-dom";
 import Fade from "react-reveal/Fade";
 
 import { PinkPurpleBackground } from "src/components/common/Backgrounds";
 import BackButton from "../components/common/BackButton";
 import PageTitle from "src/components/common/PageTitle";
 import InviteSection from "src/components/book-setting/InviteSection";
-import BookMembersInfo from "src/components/book-setting/BookMembersInfo";
-import { useParams } from "react-router-dom";
+import { BookMembersInfo } from "src/components/book-setting/BookMembersInfo";
+import LeaveBookButton from "src/components/book-setting/LeaveBookButton";
+import BookRevisionButton from "src/components/book-setting/BookRevisionButton";
 
 export interface BookIdType {
   bookId: number;
@@ -23,15 +24,9 @@ export const BookSetting = () => {
       <Fade bottom duration={1500}>
         <InviteSection bookId={bookId} />
         <BookMembersInfo bookId={bookId} />
-        <ClickableContainer>일기장 수정</ClickableContainer>
-        <ClickableContainer>일기장 나가기</ClickableContainer>
+        <BookRevisionButton bookId={bookId} />
+        <LeaveBookButton bookId={bookId} />
       </Fade>
     </PinkPurpleBackground>
   );
 };
-
-const ClickableContainer = tw.div`
-font-[notosans] text-[1.5vh] bg-white/60 rounded-lg cursor-pointer
-shadow-lg hover:shadow-none ease-in duration-300
-mx-auto mb-[1.5vh] w-[90%] p-5
-`;
