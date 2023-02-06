05f53cb6facd54a3d7fe318500b834c2590f8b8d
sojinjang <jsojin6119@gmail.com>
2023-02-06 22:21:34 +0900

    [#99] :sparkles: feat: 댓글 데이터 받아오기, 댓글 개수 보여주기

--- client/src/components/diary-detail/CommentSection.tsx
+++ client/src/components/diary-detail/CommentSection.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { API_URL } from "src/constants/API_URL";
 import { get } from "src/utils/api";
 import tw from "tailwind-styled-components";
-import { Input, InputContainer } from "../common/Input";
 import { useRecoilValue } from "recoil";
 import { accessTokenAtom } from "src/recoil/token";
 import { DiaryId } from "./DetailedDiaryContainer";
 import NewComment from "./NewComment";
+import { useDidMountEffect } from "src/utils/hooks";
 
 interface Comment {
   commentId: number;
@@ -22,11 +22,21 @@ interface Comment {
 const CommentSection = ({ diaryId }: DiaryId) => {
   const accessToken = useRecoilValue(accessTokenAtom);
   const [commentsArr, setCommentsArr] = useState<Comment[]>([]);
+  const getComments = async () => {
+    try {
+      const response = await get(API_URL.diaryComments(diaryId), "", accessToken);
+      setCommentsArr(response.data);
+    } catch (err) {
+      if (err instanceof Error) alert(err.message);
+    }
+  };
+
+  useDidMountEffect(getComments, []);
 
   return (
     <div className="w-[90%] mx-auto pb-6 md:pb-8">
       <Divider />
-      <div className="text-[1.8vh]">댓글 3</div>
+      <div className="text-[1.8vh]">댓글 {commentsArr.length}</div>
       <NewComment diaryId={diaryId} />
     </div>
   );
@@ -37,9 +47,4 @@ export default CommentSection;
 const Divider = tw.hr`
 w-full h-[2px] bg-[#C7C7C7]
 mb-4 md:mb-6
-`;
-
-const PostButton = tw.div` 
-w-[10%] ml-auto cursor-pointer text-center
-text-sm sm:text-lg text-purple-1000 hover:opacity-50 ease-in duration-300
 `;
