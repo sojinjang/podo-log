675088759a057491ddf9d5bdf064e3e5e1450dd0
sojinjang <jsojin6119@gmail.com>
2023-02-07 00:43:21 +0900

    [#99] :recycle: refactor: 닉네임, 게시글 생성 시간 공통 컴포넌트 분리

--- client/src/components/common/ProfileImg.tsx
+++
@@ -1,6 +0,0 @@
-import tw from "tailwind-styled-components";
-
-export const ProfileImg = tw.img`
-w-[45px] h-[45px] min-[390px]:w-[60px] min-[390px]:h-[60px] md:w-[63px] md:h-[63px] 
-rounded-full object-cover shadow-lg ml-4 my-4 mr-2 md:ml-6 md:my-6 md:mr-4
-`;
