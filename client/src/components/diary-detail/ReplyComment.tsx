b08ce42e75d6e8b36da7e626578dd6458d804e3c
sojinjang <jsojin6119@gmail.com>
2023-02-08 02:40:38 +0900

    [#115] :sparkles: feat: 댓글의 답글 컴포넌트 추가

---
+++ client/src/components/diary-detail/ReplyComment.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { NewCommentProps, NewComment } from "./NewComment";
+
+interface ReplyCommentProps extends NewCommentProps {
+  parentNickname: string;
+}
+export const ReplyComment = ({
+  diaryId,
+  parentNickname,
+  parentCommentId = 0,
+}: ReplyCommentProps) => {
+  return (
+    <>
+      <div className="flex text-[1.5vh] min-[390px]:text-[1.3vh]">
+        <div className="font-[notosans] font-bold">{parentNickname}</div>
+        <div className="font-[notosans]">님에게 답글 남기는중</div>
+      </div>
+      <NewComment diaryId={diaryId} parentCommentId={parentCommentId} />
+      <div className="h-[1vh]" />
+    </>
+  );
+};
