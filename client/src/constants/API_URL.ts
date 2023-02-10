227e289b328ede75095b958e1cfc996b8683ae0d
sojinjang <jsojin6119@gmail.com>
2023-02-10 21:59:45 +0900

    [#125] :truck: rename: 파일명 변경

---
+++ client/src/constants/API_URL.ts
@@ -0,0 +1,23 @@
+export const API_URL = Object.freeze({
+  kakaoLogin: "/api/login/kakao",
+  naverLogin: "/api/login/naver",
+  emailLogin: "/api/login/local",
+  refreshToken: "/api/login/silent-refresh",
+  users: "/api/users",
+  books: "/api/books",
+  inviteCode: function (bookId: number) {
+    return this.books + `/${bookId}/invtt-code`;
+  },
+  members: function (bookId: number) {
+    return this.books + `/${bookId}/members`;
+  },
+  diaryList: function (bookId: number, limit: number, startIdx: number) {
+    return this.books + `/${bookId}/diaries?limit=${limit}&start=${startIdx}`;
+  },
+  diary: "/api/diaries",
+  diaryImg: "/api/diaries/image",
+  comments: "/api/comments",
+  diaryComments: function (diaryId: number) {
+    return this.comments + `?diaryId=${diaryId}`;
+  },
+});
