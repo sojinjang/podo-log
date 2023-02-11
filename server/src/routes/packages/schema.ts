f6a6ad2cc017785abc3d6fd7cc775b83ec75d8d1
상준 안 <ahosang0056@gmail.com>
2023-02-12 00:31:08 +0900

    [#128] :sparkles: feat(package): package 구매 기능 추가

--- server/src/routes/packages/schema.ts
+++ server/src/routes/packages/schema.ts
@@ -5,4 +5,7 @@ export default {
     packageName: Joi.string().required(),
     podoPrice: Joi.number().optional(),
   }),
+  packageId: Joi.object().keys({
+    packageId: Joi.number().required(),
+  }),
 };
