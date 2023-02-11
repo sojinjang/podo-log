35b087be0006e0e25581d6cbd14fbecea1d8c98d
sojinjang <jsojin6119@gmail.com>
2023-02-11 22:15:22 +0900

    [#125] :alien: feat: 일기 한개 조회 api 추가

--- client/src/constants/API_URL.ts
+++ client/src/constants/API_URL.ts
@@ -11,8 +11,11 @@ export const API_URL = Object.freeze({
   members: function (bookId: number) {
     return this.books + `/${bookId}/members`;
   },
+  singleDiary: function (bookId: number) {
+    return this.books + `/${bookId}/diaries`;
+  },
   diaryList: function (bookId: number, limit: number, startIdx: number) {
-    return this.books + `/${bookId}/diaries?limit=${limit}&start=${startIdx}`;
+    return this.singleDiary(bookId) + `?limit=${limit}&start=${startIdx}`;
   },
   diary: "/api/diaries",
   diaryImg: function (diaryId: string) {
