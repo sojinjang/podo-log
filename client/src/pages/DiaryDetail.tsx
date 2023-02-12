84bcee21c76b938e53988ca60866c03bc2054844
sojinjang <jsojin6119@gmail.com>
2023-02-13 03:19:09 +0900

    [#137] :sparkles: feat: 스티커 편집중일때만 선택박스, 추가 아래 공간, 저장 버튼 보여주기

--- client/src/pages/DiaryDetail.tsx
+++ client/src/pages/DiaryDetail.tsx
@@ -4,13 +4,23 @@ import { useLocation } from "react-router-dom";
 import { PinkPurpleBackground } from "src/components/common/Backgrounds";
 import BackButton from "../components/common/BackButton";
 import { DetailedDiaryContainer } from "src/components/diary-detail/DetailedDiaryContainer";
+import { StickerSection } from "src/components/diary-detail/StickerSection";
+import StickerSaveBtn from "src/components/diary-detail/StickerSaveBtn";
 
 const DiaryDetail = () => {
   const location = useLocation();
+  const isEditingSticker = true;
   return (
     <PinkPurpleBackground className="overflow-y-scroll">
       <BackButton />
+      {isEditingSticker && <StickerSaveBtn />}
       <DetailedDiaryContainer data={location.state.diaryInfo} />
+      {isEditingSticker && (
+        <>
+          <StickerSection />
+          <div className="h-[23vh] " />
+        </>
+      )}
     </PinkPurpleBackground>
   );
 };
