e46fd45f017891d09f98e55734b481a151c5b2ce
sojinjang <jsojin6119@gmail.com>
2023-02-12 20:22:15 +0900

    [#125] :sparkles: feat: 사진 업로드, 삭제, 미리보기 기능 추가

---
+++ client/src/components/diary-revision/DiaryRevisionImgUpload.tsx
@@ -0,0 +1,68 @@
+import React, { useRef, useState, useEffect } from "react";
+import { useRecoilState, useResetRecoilState } from "recoil";
+
+import { diaryRevisionImgAtom } from "src/recoil/diary-revision/atom";
+import imgUploadIcon from "../../assets/icons/image.png";
+import trashCanIcon from "../../assets/icons/trash-can-white.png";
+import {
+  UploadedImg,
+  TrashCanIcon,
+  ImgUploadContainer,
+  ImgUploadIcon,
+} from "../diary/DairyImgUploadElem";
+
+interface ImgRevisionProps {
+  ogPic: string;
+}
+const DiaryRevisionImgUpload = ({ ogPic }: ImgRevisionProps) => {
+  const reader = new FileReader();
+  const imgRef = useRef<HTMLInputElement>(null);
+  const [diaryImg, setDiaryImg] = useRecoilState(diaryRevisionImgAtom);
+  const resetDiaryImg = useResetRecoilState(diaryRevisionImgAtom);
+  const [imgPreview, setImgPreview] = useState<string | ArrayBuffer | null>("");
+  const saveImgFile = () => {
+    if (imgRef?.current?.files) {
+      const file = imgRef.current.files[0];
+      setDiaryImg(file);
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setImgPreview(reader.result);
+      };
+    }
+  };
+  const deleteImgFile = () => {
+    if (typeof diaryImg === "string") URL.revokeObjectURL(diaryImg);
+    setDiaryImg("");
+  };
+
+  useEffect(() => {
+    return () => {
+      resetDiaryImg();
+    };
+  }, []);
+
+  return (
+    <React.Fragment>
+      {diaryImg ? (
+        <div className="relative overflow-auto">
+          <UploadedImg src={imgPreview ? imgPreview : imgUploadIcon}></UploadedImg>
+          <TrashCanIcon onClick={() => deleteImgFile()} src={trashCanIcon} />
+        </div>
+      ) : (
+        <ImgUploadContainer onClick={() => imgRef?.current?.click()}>
+          <ImgUploadIcon src={imgUploadIcon} />
+          <input
+            className="hidden"
+            type="file"
+            accept="image/*"
+            id="profileImg"
+            onChange={saveImgFile}
+            ref={imgRef}
+          />
+        </ImgUploadContainer>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default DiaryRevisionImgUpload;
