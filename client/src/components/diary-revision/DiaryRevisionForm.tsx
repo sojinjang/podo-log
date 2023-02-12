664e2e1c73b1569595559bc8534a37f98daed9de
sojinjang <jsojin6119@gmail.com>
2023-02-12 20:23:12 +0900

    [#125] :sparkles: feat: 기존 제목/내용 채워주기, 제목/내용 수정요청, 새 사진 post 요청

---
+++ client/src/components/diary-revision/DiaryRevisionForm.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { useRecoilValue } from "recoil";
+import { useForm } from "react-hook-form";
+
+import DiaryRevisionImgUpload from "./DiaryRevisionImgUpload";
+import { diaryRevisionImgAtom } from "src/recoil/diary-revision/atom";
+import { Img } from "src/recoil/new-diary/atom";
+import { API_URL } from "src/constants/API_URL";
+import { postFormData, patch, get } from "src/utils/api";
+import { useNavigate, useParams } from "react-router-dom";
+import { accessTokenAtom } from "src/recoil/token";
+import { DiaryForm, TitleInput, inputStyle, ContentInput } from "../diary/DiaryFormElem";
+import { DiaryInput } from "../diary/DiaryInput";
+
+interface DiaryOgInput extends DiaryInput {
+  picture: string;
+}
+
+const createDiaryForm = (diaryImg: Img) => {
+  const formData = new FormData();
+  if (diaryImg) formData.append("picture", diaryImg);
+
+  return formData;
+};
+
+const DiaryRevisionForm = () => {
+  const navigate = useNavigate();
+  const params = useParams();
+  const bookId = String(params.bookId);
+  const diaryId = String(params.diaryId);
+  const accessToken = useRecoilValue(accessTokenAtom);
+  const diaryImg = useRecoilValue(diaryRevisionImgAtom);
+  const [ogData, setOgData] = useState<DiaryOgInput | null>(null);
+  const { register, handleSubmit } = useForm<DiaryInput>({
+    mode: "onChange",
+    defaultValues: async () => {
+      const response = await get(API_URL.bookDiary(Number(bookId)), diaryId, accessToken);
+      const { title, picture, content } = response.data;
+      setOgData({ title, picture, content });
+      return { title, picture, content };
+    },
+  });
+  const onSubmitDiaryForm = async ({ title, content }: DiaryInput) => {
+    const formData = createDiaryForm(diaryImg);
+    try {
+      await patch(API_URL.diary, diaryId, { title, content }, accessToken);
+      await postFormData(API_URL.diaryImg(diaryId), formData, accessToken);
+      navigate(-1);
+    } catch (err) {
+      if (err instanceof Error) alert(err.message);
+    }
+  };
+
+  return (
+    <DiaryForm id="diary" onSubmit={handleSubmit(onSubmitDiaryForm)}>
+      <TitleInput
+        className={`${inputStyle}`}
+        placeholder="제목을 입력해주세요."
+        minLength={2}
+        maxLength={30}
+        required
+        {...register("title")}
+      />
+      <DiaryRevisionImgUpload ogPic={ogData?.picture ? ogData.picture : ""} />
+      <ContentInput
+        className={`${inputStyle}`}
+        placeholder="내용을 입력해주세요."
+        minLength={2}
+        maxLength={400}
+        required
+        {...register("content")}
+      />
+    </DiaryForm>
+  );
+};
+
+export default DiaryRevisionForm;
