733b6dc19c523f3e9e34eb1cd570635bec27a083
상준 안 <ahosang0056@gmail.com>
2023-02-16 03:10:47 +0900

    [#146] :sparkles: feat(utils): 만료시간 체크함수 개선

--- server/src/utils/check-expiration-package.ts
+++ server/src/utils/check-expiration-package.ts
@@ -11,17 +11,24 @@ export const checkExpiration = async (
 
   const [expireIds, myPackageIds, myPackagesTimeDic] = myPackagesInquiry.reduce(
     ([expireIds, ids, timeDic], cur) => {
-      const diffdays = cur.expiration.getTime() - Date.now();
       const packageId: number = cur.packageId;
-      if (diffdays < 0) expireIds.push(packageId);
-      else {
-        timeDic[packageId] = cur.expiration;
+      //cur.expiration 가 없으면 기본 스티커
+      if (!cur.expiration) {
+        timeDic[packageId] = "free";
         ids.push(packageId);
+      } else {
+        const diffdays = cur.expiration.getTime() - Date.now();
+        if (diffdays < 0) expireIds.push(packageId);
+        else {
+          timeDic[packageId] = cur.expiration;
+          ids.push(packageId);
+        }
       }
       return [expireIds, ids, timeDic];
     },
     [[], [], {}] as checkExpirationReducer
   );
+
   if (expireIds.length > 0) packageModel.deleteUserPackageByPackageIdArr(expireIds); //기다릴 필요 없을듯
 
   return [myPackageIds, myPackagesTimeDic];
