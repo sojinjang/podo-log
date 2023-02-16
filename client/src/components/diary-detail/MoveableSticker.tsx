5c0d7a43147f4cd7fe4f79b933eb5a18c74a5baf
sojinjang <jsojin6119@gmail.com>
2023-02-17 06:15:17 +0900

    [#137] :iphone: feat: pc, 모바일 같은 위치에 스티커 붙어있도록 변환 처리

--- client/src/components/diary-detail/MoveableSticker.tsx
+++ client/src/components/diary-detail/MoveableSticker.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import Moveable, { OnDragEnd } from "react-moveable";
+import Moveable, { OnDragStart, OnDrag, OnDragEnd } from "react-moveable";
 import { useSetRecoilState } from "recoil";
 import { MoveableStickerInfo, selectedStickersAtom } from "src/recoil/diary-detail/atom";
 import { useDidMountEffect } from "src/utils/hooks";
+import { convertToRelativeCoord, convertToAbsCoord } from "src/utils/convertCoord";
 
 interface DraggableStickerProps {
   sticker: MoveableStickerInfo;
@@ -14,7 +15,7 @@ const MoveableSticker = ({ sticker }: DraggableStickerProps) => {
 
   useDidMountEffect(() => {
     const targetElem = document.querySelector(`.target-${sticker.uniqueId}`) as HTMLElement;
-    targetElem.style.transform = `translate(${sticker.x}vh, ${sticker.y}vh)`;
+    targetElem.style.transform = `translate(${sticker.x}vh, ${sticker.y}vh`;
     setTargetElem(targetElem);
   }, []);
 
@@ -29,16 +30,22 @@ const MoveableSticker = ({ sticker }: DraggableStickerProps) => {
     });
   };
 
+  const handleDragStart = (e: OnDragStart) => {
+    e.set([convertToAbsCoord(sticker.x), convertToAbsCoord(sticker.y)]);
+  };
+  const handleOnDrag = (e: OnDrag) => {
+    const positionX = convertToRelativeCoord(e.beforeTranslate[0]);
+    const positionY = convertToRelativeCoord(e.beforeTranslate[1]);
+    e.target.style.transform = `translate(${positionX}vh, ${positionY}vh)`;
+  };
   const handleDragEnd = (e: OnDragEnd) => {
-    if (e.lastEvent) {
-      const positionX = e.lastEvent.beforeTranslate[0];
-      const positionY = e.lastEvent.beforeTranslate[1];
-      handleUpdateStickers({
-        ...sticker,
-        x: positionX,
-        y: positionY,
-      });
-    }
+    const positionX = convertToRelativeCoord(e.lastEvent.beforeTranslate[0]);
+    const positionY = convertToRelativeCoord(e.lastEvent.beforeTranslate[1]);
+    handleUpdateStickers({
+      ...sticker,
+      x: positionX,
+      y: positionY,
+    });
   };
 
   return (
@@ -46,9 +53,8 @@ const MoveableSticker = ({ sticker }: DraggableStickerProps) => {
       <Moveable
         target={targetElem}
         draggable={true}
-        onDrag={({ target, transform }) => {
-          target.style.transform = transform;
-        }}
+        onDragStart={handleDragStart}
+        onDrag={handleOnDrag}
         onDragEnd={handleDragEnd}
       />
       <img
