68776f868991bc3458d266f367c831b8b898afb7
sojinjang <jsojin6119@gmail.com>
2023-02-18 20:37:38 +0900

    [#159] :bug: fix: z-index 적용 안되는 문제 relative 줘서 해결

--- client/src/components/diary-detail/DeleteModal.tsx
+++ client/src/components/diary-detail/DeleteModal.tsx
@@ -29,7 +29,7 @@ const DeleteModal = ({ onClose }: ModalProps) => {
   };
 
   return (
-    <div className="z-20">
+    <div className="relative z-20">
       <Background onClick={onClose} />
       <ModalSection>
         <Fade>
