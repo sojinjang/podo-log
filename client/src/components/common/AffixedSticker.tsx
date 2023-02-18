0b1f6c39c9f11d4f8130b80eb4ba3353cbccb4f4
sojinjang <jsojin6119@gmail.com>
2023-02-18 18:58:13 +0900

    :sparkles: feat: 붙여진 스티커 컴포넌트 추가, 움직이기 기능

---
+++ client/src/components/common/AffixedSticker.tsx
@@ -0,0 +1,72 @@
+import React, { useCallback, useEffect, useState } from "react";
+import tw from "tailwind-styled-components";
+import Moveable, { OnDragStart, OnDrag, OnDragEnd } from "react-moveable";
+
+import { useDidMountEffect } from "src/utils/hooks";
+import { convertToAbsCoord, convertToRelativeCoord } from "src/utils/convertCoord";
+
+export interface AffixedStickerInfo {
+  stickedStickerId: number;
+  stickerImg: string;
+  locX: number;
+  locY: number;
+}
+
+interface AffixedStickerProps {
+  sticker: AffixedStickerInfo;
+  handleUpdateStickers: (newSticker: AffixedStickerInfo) => void;
+}
+
+export const AffixedSticker = ({ sticker, handleUpdateStickers }: AffixedStickerProps) => {
+  const [targetElem, setTargetElem] = useState<HTMLElement | SVGElement | null>(null);
+  useDidMountEffect(() => {
+    const targetElem = document.querySelector(
+      `.target-${sticker.stickedStickerId}`
+    ) as HTMLElement;
+    targetElem.style.transform = `translate(${sticker.locX}vh, ${sticker.locY}vh`;
+    setTargetElem(targetElem);
+  }, []);
+
+  const handleDragStart = (e: OnDragStart) => {
+    e.set([convertToAbsCoord(sticker.locX), convertToAbsCoord(sticker.locY)]);
+  };
+  const handleOnDrag = (e: OnDrag) => {
+    const positionX = convertToRelativeCoord(e.beforeTranslate[0]);
+    const positionY = convertToRelativeCoord(e.beforeTranslate[1]);
+    e.target.style.transform = `translate(${positionX}vh, ${positionY}vh)`;
+  };
+  const handleDragEnd = (e: OnDragEnd) => {
+    if (e.lastEvent) {
+      const positionX = convertToRelativeCoord(e.lastEvent.beforeTranslate[0]);
+      const positionY = convertToRelativeCoord(e.lastEvent.beforeTranslate[1]);
+      handleUpdateStickers({
+        ...sticker,
+        locX: positionX,
+        locY: positionY,
+      });
+    }
+  };
+
+  return (
+    <>
+      <Moveable
+        target={targetElem}
+        draggable={true}
+        onDragStart={handleDragStart}
+        onDrag={handleOnDrag}
+        onDragEnd={handleDragEnd}
+      />
+      <MoveableStickerContainer className={`target-${sticker.stickedStickerId}`}>
+        <StickerImg src={sticker.stickerImg} />
+      </MoveableStickerContainer>
+    </>
+  );
+};
+
+export const MoveableStickerContainer = tw.div`
+z-10 flex absolute cursor-pointer
+`;
+
+export const StickerImg = tw.img`
+h-[7vh]
+`;
