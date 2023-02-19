285d3b21cda1c319913ec6bc70e6eb71d0e4744b
sojinjang <jsojin6119@gmail.com>
2023-02-20 01:57:28 +0900

    [#158] :sparkles: feat: nav bar 추가

--- client/src/pages/Grape.tsx
+++ client/src/pages/Grape.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 import { DefaultBackground } from "src/components/common/Backgrounds";
+import { Navbar } from "src/components/common/NavBar";
 
 const Grape = () => {
-  return <DefaultBackground className="animated-grad-grape"></DefaultBackground>;
+  return (
+    <DefaultBackground className="animated-gradient">
+      <Navbar activeMenu="grape" />
+    </DefaultBackground>
+  );
 };
 
 export default Grape;
