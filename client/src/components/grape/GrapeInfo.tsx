c90f9fa9cf0f06f9c0b16dfe6bbefc970d9a7e67
sojinjang <jsojin6119@gmail.com>
2023-02-20 02:35:15 +0900

    [#158] :lipstick: style: 포도송이 개수, 포도알 개수 배치 및 스타일링

---
+++ client/src/components/grape/GrapeInfo.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import tw from "tailwind-styled-components";
+
+const GrapeInfo = () => {
+  return (
+    <GrapeInfoContainer>
+      <DescContainer>
+        <DescGrape className="mr-[1vh]">내가 모은 포도송이</DescGrape>
+        <DescGrape className="text-[#BADA55]">1</DescGrape>
+        <DescGrape>개</DescGrape>
+      </DescContainer>
+      <DescContainer>
+        <DescGrapeAndGrain className="text-[#BB86FC] mr-[1vh]">1</DescGrapeAndGrain>
+        <DescGrapeAndGrain className="mr-[1vh]">포도송이,</DescGrapeAndGrain>
+        <DescGrapeAndGrain className="text-[#BB86FC] mr-[1vh]">5</DescGrapeAndGrain>
+        <DescGrapeAndGrain>포도알</DescGrapeAndGrain>
+      </DescContainer>
+    </GrapeInfoContainer>
+  );
+};
+
+export default GrapeInfo;
+
+const GrapeInfoContainer = tw.div`
+flex flex-col pt-[10vh]
+`;
+
+const DescContainer = tw.div`
+flex justify-center
+`;
+
+const DescGrape = tw.p`
+font-[jua] text-[4vh] text-[#353866]
+`;
+
+const DescGrapeAndGrain = tw.p`
+font-[jua] text-[3vh] text-[#353866]
+`;
--- client/src/pages/Grape.tsx
+++ client/src/pages/Grape.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import { DefaultBackground } from "src/components/common/Backgrounds";
 import { Navbar } from "src/components/common/NavBar";
+import GrapeInfo from "../components/grape/GrapeInfo";
 
 const Grape = () => {
   return (
     <DefaultBackground className="animated-gradient">
+      <GrapeInfo />
       <Navbar activeMenu="grape" />
     </DefaultBackground>
   );
