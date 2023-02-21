948fc3c907fe92f0f337f8a3abd5acda1cc8aaf7
sojinjang <jsojin6119@gmail.com>
2023-02-21 23:49:19 +0900

    [#182] :sparkles: feat: 유저 정보 받아와서 보여주기

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { PinkPurpleBackground } from "src/components/common/Backgrounds";
 import { Navbar } from "src/components/common/NavBar";
 import PageTitle from "src/components/common/PageTitle";
+import { MyInfoSection } from "src/components/my-page/MyInfoSection";
 import WithdrawalButton from "src/components/my-page/WithdrawalButton";
 import SignOutButton from "../components/my-page/SignOutButton";
 
@@ -10,6 +11,7 @@ const MyPage = () => {
   return (
     <PinkPurpleBackground className="flex flex-col">
       <PageTitle title="마이 페이지" />
+      <MyInfoSection />
       <SignOutButton />
       <WithdrawalButton />
       <Navbar activeMenu="myPage" />
