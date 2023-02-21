d75d2f7b8c62574d279aad464977aac538a23fb4
sojinjang <jsojin6119@gmail.com>
2023-02-22 01:09:04 +0900

    [#182] :sparkles: feat: 초대코드 8자 입력창, post 요청 성공 후 일기장 리스트 페이지 이동

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -6,12 +6,14 @@ import PageTitle from "src/components/common/PageTitle";
 import { MyInfoSection } from "src/components/my-page/MyInfoSection";
 import WithdrawalButton from "src/components/my-page/WithdrawalButton";
 import SignOutButton from "../components/my-page/SignOutButton";
+import { InviteCodeSection } from "../components/my-page/InviteCodeSection";
 
 const MyPage = () => {
   return (
     <PinkPurpleBackground className="flex flex-col">
       <PageTitle title="마이 페이지" />
       <MyInfoSection />
+      <InviteCodeSection />
       <SignOutButton />
       <WithdrawalButton />
       <Navbar activeMenu="myPage" />
