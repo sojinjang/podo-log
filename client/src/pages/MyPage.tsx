dc5cad72f65fa48983292b6c933eb567f12e7aa6
sojinjang <jsojin6119@gmail.com>
2023-02-21 21:00:56 +0900

    [#182] :sparkles: feat: 페이지 제목, nav bar 추가

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import { PinkPurpleBackground } from "src/components/common/Backgrounds";
+import { Navbar } from "src/components/common/NavBar";
+import PageTitle from "src/components/common/PageTitle";
+
 const MyPage = () => {
-  return <>마이페이지</>;
+  return (
+    <>
+      <PinkPurpleBackground>
+        <PageTitle title="마이 페이지" />
+      </PinkPurpleBackground>
+      <Navbar activeMenu="myPage" />
+    </>
+  );
 };
 
 export default MyPage;
