9d58408c4fcf89b6906575d1a80cc8acbdb6fb8b
sojinjang <jsojin6119@gmail.com>
2023-02-21 18:34:32 +0900

    [#169] :iphone: style: 버튼 줄바꿈 금지

--- client/src/components/grape/MoveBtn.tsx
+++ client/src/components/grape/MoveBtn.tsx
@@ -44,6 +44,6 @@ const BtnIcon = tw.img`
 h-[2.5vh] my-auto mr-2
 `;
 
-const BtnDesc = tw.p`
-font-[jua] text-[#80A40E] text-[2.3vh]
+const BtnDesc = tw.div`
+font-[jua] text-[#80A40E] text-[2.3vh] inline
 `;
