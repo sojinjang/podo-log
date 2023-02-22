9efb3cca3d2767e5287c7d901cacf5d553764123
sojinjang <jsojin6119@gmail.com>
2023-02-22 18:24:34 +0900

    [#186] :sparkles: feat: 새 비밀번호 - 새 비밀번호 확인 일치 검사 후 수정 요청

---
+++ client/src/components/edit-info/EditPassword.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import tw from "tailwind-styled-components";
+import { useRecoilValue } from "recoil";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+import { accessTokenAtom } from "src/recoil/token";
+import { API_URL } from "src/constants/API_URL";
+import { patch } from "src/utils/api";
+import { PRIVATE_ROUTE } from "src/router/ROUTE_INFO";
+import { Input, InputContainer } from "src/components/common/Input";
+import PurpleButton from "src/components/common/PurpleButton";
+
+interface PasswordInput {
+  readonly password: string;
+  readonly newPassword: string;
+  readonly newPwConfirm?: string | undefined;
+}
+
+const EditPassword = () => {
+  const accessToken = useRecoilValue(accessTokenAtom);
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<PasswordInput>({
+    mode: "onChange",
+  });
+  const pwConfirmRegister = register("newPwConfirm", {
+    validate: (pwconfirm: string | undefined) => {
+      const { newPassword } = getValues();
+      return newPassword === pwconfirm || "비밀번호가 일치하지 않습니다.";
+    },
+  });
+
+  const onSubmitEdit = async ({ password, newPassword }: PasswordInput) => {
+    try {
+      await patch(API_URL.users, "", { password, newPassword }, accessToken);
+      navigate(PRIVATE_ROUTE.myPage.path);
+    } catch (err) {
+      if (err instanceof Error) alert(err.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmitEdit)}>
+      <InputContainer>
+        <Input
+          placeholder="old password"
+          type="password"
+          minLength={4}
+          required
+          {...register("password")}
+        />
+      </InputContainer>
+      <InputContainer>
+        <Input
+          placeholder="new password"
+          type="password"
+          minLength={4}
+          required
+          {...register("newPassword")}
+        />
+      </InputContainer>
+      <div className="w-[65%] mx-auto">
+        <InputContainer className="w-full">
+          <Input
+            placeholder="confirm new password"
+            type="password"
+            minLength={4}
+            required
+            {...pwConfirmRegister}
+          />
+        </InputContainer>
+        {errors.newPwConfirm && <PwConfirmMsg>{errors.newPwConfirm.message}</PwConfirmMsg>}
+      </div>
+      <PurpleButton
+        description="수정하기"
+        wrapperStyle="mt-[1.5vh] w-full"
+        buttonStyle="w-[65%]"
+      />
+    </form>
+  );
+};
+
+export default EditPassword;
+
+const PwConfirmMsg = tw.p`
+font-[notosans] text-red-600 text-xs sm:text-base mt-1 ml-[5px]
+`;
--- client/src/pages/EditInfo.tsx
+++ client/src/pages/EditInfo.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
+import { PinkPurpleBackground } from "src/components/common/Backgrounds";
+import BackButton from "../components/common/BackButton";
+import PageTitle from "src/components/common/PageTitle";
+import { UnclickableContainer } from "src/components/common/UnclickableContainer";
+import EditNickname from "../components/edit-info/EditNickname";
+import EditPassword from "../components/edit-info/EditPassword";
+
 const EditInfo = () => {
-  return <>내 정보 수정</>;
+  return (
+    <PinkPurpleBackground className="flex flex-col">
+      <BackButton />
+      <PageTitle title="내 정보 수정" />
+      <UnclickableContainer className="m-auto w-[70%] h-[50vh]">
+        <EditNickname />
+        <EditPassword />
+      </UnclickableContainer>
+    </PinkPurpleBackground>
+  );
 };
 
 export default EditInfo;
