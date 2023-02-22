ff98366206f64501a052c1337e1afcc158a23d6e
sojinjang <jsojin6119@gmail.com>
2023-02-23 02:31:01 +0900

    [#186] :sparkles: feat: 프로필사진 추가 기능 추가

---
+++ client/src/components/edit-info/EditProfile.tsx
@@ -0,0 +1,94 @@
+import React, { useState, useRef } from "react";
+import tw from "tailwind-styled-components";
+import { useRecoilValue } from "recoil";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { accessTokenAtom } from "src/recoil/token";
+import { postFormData, del } from "src/utils/api";
+import DefaultProfileImg from "src/assets/icons/default_profile.png";
+import { PRIVATE_ROUTE } from "src/router/ROUTE_INFO";
+import { API_URL } from "src/constants/API_URL";
+
+import PurpleButton from "src/components/common/PurpleButton";
+
+const EditProfile = () => {
+  const accessToken = useRecoilValue(accessTokenAtom);
+  const imgRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const reader = new FileReader();
+  const [profileImg, setProfileImg] = useState(location.state.myInfo.profile);
+  const [imgPreview, setImgPreview] = useState<string | ArrayBuffer | null>("");
+
+  const saveImgFile = () => {
+    if (imgRef?.current?.files) {
+      const file = imgRef.current.files[0];
+      setProfileImg(file);
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setImgPreview(reader.result);
+      };
+    }
+  };
+  const deleteImgFile = () => {
+    if (typeof profileImg === "string") URL.revokeObjectURL(profileImg);
+    setProfileImg("");
+  };
+  const createFormData = () => {
+    const formData = new FormData();
+    formData.append("profile", profileImg);
+    return formData;
+  };
+  const onSubmitEdit = async () => {
+    try {
+      await postFormData(API_URL.profile, createFormData(), accessToken);
+      navigate(PRIVATE_ROUTE.myPage.path);
+    } catch (err) {
+      if (err instanceof Error) alert(err.message);
+    }
+  };
+
+  return (
+    <div className="m-auto flex flex-col text-center">
+      <ProfileImg src={profileImg && profileImg ? imgPreview : DefaultProfileImg} />
+      {!profileImg && (
+        <ProfileImgDescription htmlFor="profileImg">프로필 이미지 추가</ProfileImgDescription>
+      )}
+      {profileImg && (
+        <ProfileImgDescription onClick={deleteImgFile}>
+          프로필 이미지 삭제
+        </ProfileImgDescription>
+      )}
+      <input
+        className="hidden"
+        type="file"
+        accept="image/*"
+        id="profileImg"
+        onChange={saveImgFile}
+        ref={imgRef}
+      />
+      <ButtonContainer onClick={onSubmitEdit}>
+        <PurpleButton
+          description="이미지 수정하기"
+          wrapperStyle="w-full"
+          buttonStyle="w-[65%]"
+        />
+      </ButtonContainer>
+    </div>
+  );
+};
+
+export default EditProfile;
+
+const ProfileImg = tw.img`
+w-[10vh] h-[10vh] object-cover shadow-lg mx-auto my-2 rounded-full bg-white
+`;
+
+const ProfileImgDescription = tw.label`
+cursor-pointer text-purple-1000 text-[1.4vh]
+drop-shadow-lg hover:drop-shadow-none transition duration-300 ease-in-out
+`;
+
+const ButtonContainer = tw.div`
+inline-block w-auto mt-[1vh] mb-[1.5vh]
+`;
--- client/src/pages/EditInfo.tsx
+++ client/src/pages/EditInfo.tsx
@@ -6,13 +6,15 @@ import PageTitle from "src/components/common/PageTitle";
 import { UnclickableContainer } from "src/components/common/UnclickableContainer";
 import EditNickname from "../components/edit-info/EditNickname";
 import EditPassword from "../components/edit-info/EditPassword";
+import EditProfile from "src/components/edit-info/EditProfile";
 
 const EditInfo = () => {
   return (
     <PinkPurpleBackground className="flex flex-col">
       <BackButton />
       <PageTitle title="내 정보 수정" />
-      <UnclickableContainer className="m-auto w-[70%] h-[50vh]">
+      <UnclickableContainer className="m-auto w-[70%] py-[5vh]">
+        <EditProfile />
         <EditNickname />
         <EditPassword />
       </UnclickableContainer>
