bc6c280e2cb4dee50fc01d183fcd9bf2c2f915d7
sojinjang <jsojin6119@gmail.com>
2023-02-23 06:08:25 +0900

    [#186] :sparkles: feat: 기존 프사 컴포넌트, 새 프사 컴포넌트 분리

---
+++ client/src/components/edit-info/NewProfile.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import DefaultProfileImg from "src/assets/icons/default_profile.png";
+import { ProfileImg, ProfileImgDescription } from "src/components/common/Profile";
+
+interface NewProfileProps {
+  profileImg: string | Blob;
+  imgPreview: string | ArrayBuffer | null;
+  deleteImgFile: () => void;
+}
+
+const NewProfile = ({ profileImg, imgPreview, deleteImgFile }: NewProfileProps) => {
+  return (
+    <>
+      <ProfileImg src={profileImg ? imgPreview : DefaultProfileImg} />
+      {profileImg ? (
+        <ProfileImgDescription onClick={deleteImgFile}>
+          프로필 이미지 수정
+        </ProfileImgDescription>
+      ) : (
+        <ProfileImgDescription htmlFor="profileImg">프로필 이미지 추가</ProfileImgDescription>
+      )}
+    </>
+  );
+};
+
+export default NewProfile;
