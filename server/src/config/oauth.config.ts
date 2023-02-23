09b49b5ef5441f2b290ea0766fdb37cca7b3ad68
ahosang <70270186+ahosang@users.noreply.github.com>
2023-02-23 20:03:22 +0900

    🚚 chore: server 주소 추가 (#188)

--- server/src/config/oauth.config.ts
+++ server/src/config/oauth.config.ts
@@ -1,10 +1,10 @@
 export const kakao = {
   clientID: process.env.KAKAO_API_KEY as string,
-  callbackURL: `/api/login/kakao/oauth`,
+  callbackURL: `${process.env.PODOLOG_SERVER_URL}/api/login/kakao/oauth`,
 };
 
 export const naver = {
   clientID: process.env.NAVER_API_ID,
   clientSecret: process.env.NAVER_API_SECRET,
-  callbackURL: `/api/login/naver/oauth`,
+  callbackURL: `${process.env.PODOLOG_SERVER_URL}/api/login/naver/oauth`,
 };
