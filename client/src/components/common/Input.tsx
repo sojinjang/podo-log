be420b32b18b2f3446bbec5ad2fee55a17cfe107
sojinjang <jsojin6119@gmail.com>
2023-02-24 20:08:44 +0900

    [#189] :iphone: style: input창 크기 조절

--- client/src/components/common/Input.tsx
+++ client/src/components/common/Input.tsx
@@ -6,9 +6,9 @@ w-[80%] py-[2vh] mt-[2vh] min-[390px]:mt-[6vh] mx-auto
 `;
 
 export const Input = tw.input`
-font-sans bg-transparent text-[1.3vh]
+font-sans bg-transparent text-[1.5vh]
 `;
 
 export const InputContainer = tw.div`
-w-[65%] flex flex-col rounded-md bg-white/40 p-[0.8vh] mx-auto mt-[1.5vh]
+w-[65%] flex flex-col rounded-md bg-white/40 p-[1vh] mx-auto mt-[1.5vh]
 `;
