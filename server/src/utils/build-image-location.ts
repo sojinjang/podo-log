1d02f1129c01d14b079fc2ee7df9c93048338b1a
상준 안 <ahosang0056@gmail.com>
2023-02-24 14:44:30 +0900

    :bug: fix(utils): buildImgLocation 조건 수정

--- server/src/utils/build-image-location.ts
+++ server/src/utils/build-image-location.ts
@@ -9,13 +9,15 @@ export const buildImgLocation = (
   if (Array.isArray(data)) {
     return data.map((cur) => {
       keys.forEach((key) => {
-        if (cur[key] !== "없음" && cur[key] !== undefined) cur[key] = S3AccessURL + cur[key];
+        if (cur[key] !== "없음" && cur[key].slice(0, 4) !== "http")
+          cur[key] = S3AccessURL + cur[key];
       });
       return cur;
     });
   } else {
     keys.forEach((key) => {
-      if (data[key] !== "없음" && data[key] !== undefined) data[key] = S3AccessURL + data[key];
+      if (data[key] !== "없음" && data[key].slice(0, 4) !== "http")
+        data[key] = S3AccessURL + data[key];
     });
     return data;
   }
