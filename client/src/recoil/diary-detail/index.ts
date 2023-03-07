45b0f4736f4bf99527652f1bb81b653337b16c2e
sojinjang <jsojin6119@gmail.com>
2023-03-07 19:16:51 +0900

    [#194] :sparkles: feat: comment를 selector getter로 가져와서 diaryId의 derived value로 만듦

--- client/src/recoil/diary-detail/index.ts
+++ client/src/recoil/diary-detail/index.ts
@@ -1,1 +1,23 @@
+import { selector } from "recoil";
+
+import { API_URL } from "src/constants/API_URL";
+import { focusedDiaryIdAtom } from "src/recoil/diary-detail/atom";
+import { accessTokenAtom } from "../token";
+import * as api from "src/utils/api";
+
 export { focusedDiaryIdAtom, isDeleteModalVisibleAtom, deleteInfoAtom } from "./atom";
+
+export const getComments = selector({
+  key: "getComments",
+  get: async ({ get }) => {
+    const diaryId = get(focusedDiaryIdAtom);
+    const accessToken = get(accessTokenAtom);
+
+    try {
+      const response = await api.get(API_URL.diaryComments(diaryId), "", accessToken);
+      return response.data;
+    } catch (err) {
+      if (err instanceof Error) alert(err.message);
+    }
+  },
+});
