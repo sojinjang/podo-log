bc17084d19eaeccd96830233e879788bbe30ce2d
sojinjang <jsojin6119@gmail.com>
2023-03-16 15:03:41 +0900

    [#211] :sparkles: feat: 이미지 용량, 크기 제한두고 압축하는 기능 추가

---
+++ client/src/utils/compressImg.ts
@@ -0,0 +1,14 @@
+import imageCompression from "browser-image-compression";
+
+export const compressImg = async (image: File) => {
+  const options = {
+    maxSizeMb: 1,
+    maxWidthOrHeight: 1000,
+  };
+  try {
+    const blob = await imageCompression(image, options);
+    return new File([blob], blob.name);
+  } catch (err) {
+    if (err instanceof Error) alert(err.message);
+  }
+};
