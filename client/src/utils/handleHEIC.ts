d50aa0e513324a0b78ebe443b45b80d416460cf6
sojinjang <jsojin6119@gmail.com>
2023-03-16 15:04:14 +0900

    [#211] :bug: fix: 이미지 압축 전 이미지로 인식하도록 타입 줘서 파일 객체로 만들기

--- client/src/utils/handleHEIC.ts
+++ client/src/utils/handleHEIC.ts
@@ -6,5 +6,7 @@ export const isHEICFile = (file: File): boolean => {
 
 export const convertHEICToJPG = async (file: File) => {
   const jpgBlob = await heic2any({ blob: file, toType: "image/jpeg" });
-  return new File([jpgBlob as BlobPart], file.name.split(".")[0] + ".jpg");
+  return new File([jpgBlob as BlobPart], file.name.split(".")[0] + ".jpg", {
+    type: "image/jpeg",
+  });
 };
