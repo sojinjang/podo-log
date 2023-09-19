9e7d7838d33881b8b61d47189dfa750144f6f184
ahosang <70270186+ahosang@users.noreply.github.com>
2023-09-19 13:49:09 +0900

    🐛 fix: validation 수정
    기존 invtt-code 요청의 의미없는 validation 삭제

--- server/src/api/routes/invtt-code/invtt-code.validation.ts
+++ server/src/api/routes/invtt-code/invtt-code.validation.ts
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export default {
   bookId: Joi.object().keys({
-    bookId: Joi.number().required().min(1).max(20),
+    bookId: Joi.number().required(),
   }),
   joinBook: Joi.object().keys({
     invttCode: Joi.string().required().length(8),
